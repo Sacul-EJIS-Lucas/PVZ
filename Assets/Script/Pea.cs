b99bacac82a0cdc9bcf1eaa56a2d6ea17a073305
EJIS <lucas.fan@pacificacademy.org>
2026-04-17 00:35:29 -0700

    1

--- Assets/Script/Pea.cs
+++ Assets/Script/Pea.cs
@@ -1,16 +1,23 @@
+// Pea.cs
 using UnityEngine;
 
 public class Pea : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float speed = 5f;
+    public int damage = 20;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (transform.position.x > 8f) Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Zombie"))
+        {
+            other.GetComponent<ZombieBase>()?.TakeDamage(damage);
+            Destroy(gameObject);
+        }
     }
 }
